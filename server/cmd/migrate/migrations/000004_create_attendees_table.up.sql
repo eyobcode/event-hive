51d626a20977e273a40b26ade9a3eef681fa29cf
eyobScript <eyobbmulugeta@gmail.com>
2025-11-11 03:24:20 +0300

    migrations

---
+++ server/cmd/migrate/migrations/000004_create_attendees_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS attendees (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    event_id BIGINT NOT NULL,
+    checked_in BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_attendee_user
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_attendee_event
+    FOREIGN KEY (event_id) REFERENCES events(id)
+    ON DELETE CASCADE
+);
---
+++ server/cmd/migrate/migrations/000005_create_tickets_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS tickets (
+    id BIGSERIAL PRIMARY KEY,
+    event_id BIGINT NOT NULL,
+    ticket_type VARCHAR(50),
+    price NUMERIC(10,2),
+    quantity INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_ticket_event
+    FOREIGN KEY (event_id) REFERENCES events(id)
+    ON DELETE CASCADE
+);
