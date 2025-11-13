6817069bef3efc7b4a7df590c3fad1dbdbc015ec
eyobScript <eyobbmulugeta@gmail.com>
2025-11-13 15:55:02 +0300

    event handler

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -3,11 +3,21 @@ package main
 import (
 	"net/http"
 
+	"github.com/eyobcode/event-hive/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func (app *application) routes() http.Handler {
 	router := gin.Default()
-	// define routes here later
+	api := router.Group("/api")
+	{
+		event := api.Group("/events")
+		{
+			h := handlers.EventHandler{DB: app.DB}
+			event.POST("/", h.CreateEvent)
+			event.GET("/", h.GetAllEvents)
+		}
+	}
+
 	return router
 }
