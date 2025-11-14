19e9b229c99804eacf03a9e5a5637b008cb45cec
eyobScript <eyobbmulugeta@gmail.com>
2025-11-14 05:17:54 +0300

    error

--- server/internal/models/organizer.go
+++ server/internal/models/organizer.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -15,4 +16,17 @@ type Organizer struct {
 	OrganizationName string    `json:"organization_name" binding:"required"`
 	ContactEmail     string    `json:"contact_email"`
 	CreatedAt        time.Time `json:"created_at"`
+}
+
+func (m *OrganizerModel) EnsureOrganizer(userId int) (int, error) {
+	var id int
+	err := m.DB.QueryRow("SELECT id FROM organizers WHERE user_id=$1", userId).Scan(&id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, nil
+		}
+		// Some other database error
+		return 0, err
+	}
+	return id, nil
 }
