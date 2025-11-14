e20318f5e7cab93d51b5a06fe6e20093163425e4
eyobScript <eyobbmulugeta@gmail.com>
2025-11-14 05:18:53 +0300

    check if the category exist

--- server/internal/models/catagory.go
+++ server/internal/models/catagory.go
@@ -10,3 +10,12 @@ type Category struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
+
+func (m *CategoryModel) CategoryExist(id int) (bool, error) {
+	var exists bool
+	err := m.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM categories WHERE id=$1)", id).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
